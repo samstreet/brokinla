a6b7986861f1f9413eaacbc89c8311e8ddb25a71
samstreet <samstreet@users.noreply.github.com>
2025-03-28 22:29:56 +0000

    Initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Footer from './components/footer'
 import Header from './components/header'
 import Hero from './components/hero';
 import { metadata } from './layout';
-import Image from 'next/image';
 
 
 export default function Home() {
