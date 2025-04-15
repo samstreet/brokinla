ca2bd63f3ab00a1708fa1928b38bba802f57e3ed
samstreet <samstreet@users.noreply.github.com>
2025-04-15 23:16:07 +0100

    Initial

--- src/app/dogs/[dog]/page.tsx
+++ src/app/dogs/[dog]/page.tsx
@@ -2,6 +2,7 @@ import { type SanityDocument } from "next-sanity";
 import { client } from "@/sanity/client";
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
+import Shows from "@/app/components/shows";
 
 const POST_QUERY = `*[_type == "dogs" && slug == $dog][0]{
   name,
@@ -19,39 +20,16 @@ export default async function PostPage({
 }: {
   params: Promise<{ dog: string }>;
 }) {
-  const dog = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
+  const dog = await client.fetch(POST_QUERY, await params, options);
+  console.log(dog);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header/>
     
       <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
         <h1 className="text-4xl font-bold mb-8">{dog.name}</h1>
-        {dog.shows?.forEach((show, showIndex: number) => {
-          <div className="relative flex w-96 flex-col rounded-lg border border-slate-200 bg-white shadow-sm" key={showIndex}>
-            <nav className="flex min-w-[240px] flex-col gap-1 p-1.5">
-              <div
-                role="button"
-                className="text-slate-800 flex w-full items-center rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100"
-              >
-                <div className="mr-4 grid place-items-center">
-                  <img
-                    alt="candice"
-                    src="https://docs.material-tailwind.com/img/face-1.jpg"
-                    className="relative inline-block h-12 w-12 !rounded-full  object-cover object-center"
-                  />
-                </div>
-                <div>
-                  <h6 className="text-slate-800 font-medium">
-                    {show.name}
-                  </h6>
-                  <p className="text-slate-500 text-sm">
-                    {show.name}
-                  </p>
-                </div>
-              </div>
-            </nav>
-          </div>
-        })}
+        <Shows dog={dog} />
       </main>
 
       <Footer />
